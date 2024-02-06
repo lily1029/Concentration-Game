7d6e011cbf28c7d19e0d58b3e758fb292530e564
lily1029 <lily.zhao1029@gmail.com>
2024-02-06 00:06:16 -0800

    Concentration Game Update

---
+++ js/app.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const cardsColletion = [
+        {
+            name: 'dog', 
+            image: 'images/dog.png'
+        }, 
+        {
+            name: 'dog', 
+            image: 'images/dog.png'
+        },
+        {
+            name: 'dog', 
+            image: 'images/dog.png'
+        },
+        {
+            name: 'dog', 
+            image: 'images/dog.png'
+        },
+        {
+            name: 'dog', 
+            image: 'images/dog.png'
+        },
+        {
+            name: 'dog', 
+            image: 'images/dog.png'
+        }
+    ]
+
+    function initiateBoard(){}
+
+})
